9325c3f57a9732da76b2e345eb5d7c6e7677a727
cclaiborne <claiborne206@gmail.com>
2014-02-23 19:13:51 -0800

    added case for 0 and changed cases for 1 and 2 to be correct

--- lib/Fibonacci.rb
+++ lib/Fibonacci.rb
@@ -1,13 +1,17 @@
 class Fibonacci
   def initialize()
   end
+
   def getfibo(n)
-    if (n == 0 || n == 1)
+    if (n == 0)
+      return 0
+    elsif (n == 1 || n == 2)
       return 1
     else
       return getfibo(n-2) + getfibo(n-1)
     end
   end
 end
+
 
 
--- test/fibo_test.rb
+++ test/fibo_test.rb
@@ -6,10 +6,11 @@ describe "a sequence of fibonacci numbers" do
   #given an nth Fibonacci number
   @fibo = Fibonacci.new()
   #then the nth Fibonacci number is equivalent to the correct Fibonaaci number in sequence.
-  assert_equal 1, @fibo.getfibo(0)
+  assert_equal 0, @fibo.getfibo(0)
   assert_equal 1, @fibo.getfibo(1)
-  assert_equal 2, @fibo.getfibo(2)
-  assert_equal 3, @fibo.getfibo(3)
-  assert_equal 5, @fibo.getfibo(4)
+  assert_equal 1, @fibo.getfibo(2)
+  assert_equal 2, @fibo.getfibo(3)
+  assert_equal 3, @fibo.getfibo(4)
+  assert_equal 5, @fibo.getfibo(5)
   end
 end
